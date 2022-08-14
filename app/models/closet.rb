436e777553fc2f152aad2ecae1449e6df9f3378e
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-08-14 15:43:03 +0900

    アソシエーションの設定

--- app/models/closet.rb
+++ app/models/closet.rb
@@ -1,2 +1,11 @@
 class Closet < ApplicationRecord
+  belongs_to :user
+
+  validates :image,     presence: true
+  validates :brand,     presence: true
+  validates :price,     presence: true
+  validates :genre_id,  presence: true
+  validates :color_id,  presence: true
+  validates :season_id, presence: true
+  validates :size_id,   presence: true
 end

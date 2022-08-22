ddf336eb92165cbb03df2d017d8805477f5e0d1f
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-08-22 13:17:48 +0900

    ビューの調整

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,10 +1,10 @@
 class Color < ActiveHash::Base
   self.data = [
-    { id: 0, name: 'カラー' }, { id: 1, name: 'ホワイト' }, { id: 2, name: 'ブラック' },
-    { id: 3, name: 'グレー' }, { id: 4, name: 'ブラウン' }, { id: 5, name: 'ブルー' },
-    { id: 6, name: 'グリーン' }, { id: 7, name: 'イエロー' }, { id: 8, name: 'オレンジ' },
-    { id: 9, name: 'レッド' }, { id: 10, name: 'パープル' }, { id: 11, name: 'ピンク' },
-    { id: 12, name: 'ベージュ' }, { id: 13, name: 'その他' },
+    { id: 0, name: 'カラー' }, { id: 1, name: 'ホワイト', backcolor: "#fff" }, { id: 2, name: 'ブラック', backcolor: "#000" },
+    { id: 3, name: 'グレー', backcolor: "#808080" }, { id: 4, name: 'ブラウン', backcolor: "#660000" }, { id: 5, name: 'ブルー', backcolor: "#0000CD" },
+    { id: 6, name: 'グリーン', backcolor: "#006400" }, { id: 7, name: 'イエロー', backcolor: "#FFFF00" }, { id: 8, name: 'オレンジ', backcolor: "#fd7e00" },
+    { id: 9, name: 'レッド', backcolor: "#FF0000" }, { id: 10, name: 'パープル', backcolor: "#8B008B" }, { id: 11, name: 'ピンク', backcolor: "#FF82B2" },
+    { id: 12, name: 'ベージュ', backcolor: "#FFE4C4" }, { id: 13, name: 'その他', backcolor: "#d3d3d3" },
   ]
   include ActiveHash::Associations
   has_many :closets

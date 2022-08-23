d512321fd06d0845e1923e4a7d839117cd8c2b7a
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-08-23 12:08:29 +0900

    着用回数の非同期登録機能の実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,10 @@
 class FavoritesController < ApplicationController
+  def create
+    @closet = Closet.find(params[:closet_id])
+    @favorite = Favorite.create(user_id: current_user.id, closet_id: @closet.id)
+    
+    if @favorite.save
+      render :favorite
+    end
+  end
 end

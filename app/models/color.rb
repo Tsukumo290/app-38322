b511386311c1720ee54465225b4c29578c5cf6ff
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-08-25 11:50:37 +0900

    カラーの調整

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,10 +1,10 @@
 class Color < ActiveHash::Base
   self.data = [
-    { id: 0, name: 'カラー' }, { id: 1, name: 'ホワイト' }, { id: 2, name: 'ブラック' },
-    { id: 3, name: 'グレー' }, { id: 4, name: 'ブラウン' }, { id: 5, name: 'ブルー' },
-    { id: 6, name: 'グリーン' }, { id: 7, name: 'イエロー' }, { id: 8, name: 'オレンジ' },
-    { id: 9, name: 'レッド' }, { id: 10, name: 'パープル' }, { id: 11, name: 'ピンク' },
-    { id: 12, name: 'ベージュ' }, { id: 13, name: 'その他' },
+    { id: 0, name: 'カラー' }, { id: 1, name: 'ホワイト' }, { id: 2, name: 'イエロー' },
+    { id: 3, name: 'オレンジ' }, { id: 4, name: 'レッド' }, { id: 5, name: 'ピンク' },
+    { id: 6, name: 'パープル' }, { id: 7, name: 'ブルー' }, { id: 8, name: 'グリーン' },
+    { id: 9, name: 'ベージュ' }, { id: 10, name: 'ブラウン' }, { id: 11, name: 'グレー' },
+    { id: 12, name: 'ブラック' }, { id: 13, name: 'その他' },
   ]
   include ActiveHash::Associations
   has_many :closets

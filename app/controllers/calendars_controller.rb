cc672fb68ecdfa6c6ab136fd4888a02e246e996b
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-08-26 15:30:23 +0900

    newアクションの設定

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,4 +1,22 @@
 class CalendarsController < ApplicationController
   def index
   end
+
+  def new
+    @calendar = Calendar.new
+  end
+
+  def create
+    @calendar = Calendar.new(calendar_params)
+    if @calendar.save
+      redirect_to calendars_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def calendar_params
+    params.require(:calendar).permit(image: []).merge(user_id: current_user.id)
+  end
 end

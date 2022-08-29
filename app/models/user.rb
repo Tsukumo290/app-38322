4bfcba4afc8baf2ff2abc09ebc375e24cc28f70f
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-08-29 19:32:23 +0900

    天気予報の取得と表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,11 +20,11 @@ class User < ApplicationRecord
   end
 
   def prefecture_name
-  JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  JpPrefecture::Prefecture.find(code: prefecture_code).try(:name_e)
   end
 
   def prefecture_name=(prefecture_name)
-    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+    self.prefecture_code = JpPrefecture::Prefecture.find(name_e: prefecture_name).code
   end
 
 end

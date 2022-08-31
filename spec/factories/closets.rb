f4879e2710057cf3dda0ae7f8819f59d8691b25d
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-08-31 15:47:36 +0900

    closetモデル単体テスト実施

--- spec/factories/closets.rb
+++ spec/factories/closets.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :closet do
-    
+    brand     { 'hoge' }
+    price     { 300 }
+    genre_id  { 1 }
+    color_id  { 1 }
+    season_id { 1 }
+    size_id   { 1 }
+    association :user
   end
 end

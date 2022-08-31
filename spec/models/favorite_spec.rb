56b28a2841c799deacdf72efa2a34a0b6e7e0bab
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-08-31 17:42:29 +0900

    favoriteモデルの単体テストの実施

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @favo = FactoryBot.build(:favorite)
+  end
+
+  describe '着用回数機能' do
+    context '回数を増やすことができる場合' do
+      it "user_idとcloset_idがあれば保存できる" do
+        expect(@favo).to be_valid
+      end
+    end
+
+    context '回数を増やすことができない場合' do
+      it 'userが紐付いていないと保存できない' do
+        @favo.user = nil
+        @favo.valid?
+        expect(@favo.errors.full_messages).to include('User must exist')
+      end
+
+      it 'closetが紐付いていないと保存できない' do
+        @favo.closet = nil
+        @favo.valid?
+        expect(@favo.errors.full_messages).to include('Closet must exist')
+      end
+    end
+  end
 end

e2e1a8226da6dad1ec40b4c234f81ba4ba7e23b5
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-08-31 11:11:07 +0900

    エラーメッセージの表示

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -5,4 +5,15 @@ class Calendar < ApplicationRecord
   validates :image, presence: true
   validates :start_time, presence: true
 
+  FILE_NUMBER_LIMIT = 6
+
+  validate :validate_number_of_files
+
+  private
+
+  def validate_number_of_files
+    return if image.length <= FILE_NUMBER_LIMIT
+    errors.add(:image, "に添付できる画像は#{FILE_NUMBER_LIMIT}件までです。")
+  end
+
 end
--- app/models/closet.rb
+++ app/models/closet.rb
@@ -10,10 +10,14 @@ class Closet < ApplicationRecord
   belongs_to :size
 
   validates :image,     presence: true
-  validates :brand,     presence: true
-  validates :price,     presence: true
+  validates :brand,     presence: true, length: { maximum: 40 }
   validates :genre_id,  presence: true, numericality: { other_than: 0, message: "can't be blank" }
   validates :color_id,  presence: true, numericality: { other_than: 0, message: "can't be blank" }
   validates :season_id, presence: true, numericality: { other_than: 0, message: "can't be blank" }
   validates :size_id,   presence: true, numericality: { other_than: 0, message: "can't be blank" }
+  with_options presence: true, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' } do
+    validates :price,
+              numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
+                              message: 'out of setting range' }
+  end
 end

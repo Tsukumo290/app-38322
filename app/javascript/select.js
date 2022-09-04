0e5df26fa6898707c28dc7e7a900a771b52d508a
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-09-04 13:35:42 +0900

    カテゴリーごとにシーズンの表示が変わるよう修正

--- app/javascript/select.js
+++ app/javascript/select.js
@@ -10,6 +10,7 @@ $(document).on('change', '#genre', function() {
       $('#size option[value = 4]').css('display', 'none');
       $('#size option[value = 5]').css('display', 'none');
       $('#size option[value = 6]').css('display', 'none');
+
       $('#size option[value = 7]').css('display', 'block');
       $('#size option[value = 8]').css('display', 'block');
       $('#size option[value = 9]').css('display', 'block');
@@ -17,6 +18,13 @@ $(document).on('change', '#genre', function() {
       $('#size option[value = 11]').css('display', 'block');
       $('#size option[value = 12]').css('display', 'block');
       $('#size option[value = 13]').css('display', 'block');
+
+      $('#season option[value = 5]').css('display', 'block');
+
+      $('#season option[value = 1]').css('display', 'none');
+      $('#season option[value = 2]').css('display', 'none');
+      $('#season option[value = 3]').css('display', 'none');
+      $('#season option[value = 4]').css('display', 'none');
     } else {
       $('#size option[value = 7]').css('display', 'none');
       $('#size option[value = 8]').css('display', 'none');
@@ -25,11 +33,19 @@ $(document).on('change', '#genre', function() {
       $('#size option[value = 11]').css('display', 'none');
       $('#size option[value = 12]').css('display', 'none');
       $('#size option[value = 13]').css('display', 'none');
+
       $('#size option[value = 1]').css('display', 'block');
       $('#size option[value = 2]').css('display', 'block');
       $('#size option[value = 3]').css('display', 'block');
       $('#size option[value = 4]').css('display', 'block');
       $('#size option[value = 5]').css('display', 'block');
       $('#size option[value = 6]').css('display', 'block');
+
+      $('#season option[value = 5]').css('display', 'none');
+
+      $('#season option[value = 1]').css('display', 'block');
+      $('#season option[value = 2]').css('display', 'block');
+      $('#season option[value = 3]').css('display', 'block');
+      $('#season option[value = 4]').css('display', 'block');
     };
 });
